6da2a6b68164dcb4162d73803270073e34fb3995
Ming0o <isha3992@gmail.com>
2023-05-18 12:23:02 +0900

    Feat: 6주차 Redux 더미리듀서 설정

--- nacho/week6/src/main.jsx
+++ nacho/week6/src/main.jsx
@@ -3,10 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+
+// 스토어 생성
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter><App /></BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
